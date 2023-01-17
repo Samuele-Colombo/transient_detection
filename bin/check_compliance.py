68dd5c062c47c373abf7ef53570418232d41b611
Samuele-Colombo <samuele.colombo1@studenti.unimi.it>
2023-01-17 10:11:26 -0500

    Flush to file after each write

--- bin/check_compliance.py
+++ bin/check_compliance.py
@@ -24,13 +24,14 @@ def check_compliance(args):
                 raise ki
             except:
                 f.write(" ".join([genuine, simulated]) + "\n")
+                f.flush()
                 continue
 
             # I_dat = astropy_table.Table.read(genuine, hdu=1, format=)
             # F_dat = astropy_table.Table.read(simulated, hdu=1)
             if any(key not in dat.colnames for dat in (I_dat, F_dat) for key in keys):
                 f.write(" ".join([genuine, simulated]) + "\n")
-    
+                f.flush()
 
 if __name__ == "__main__":
     args = parse()
