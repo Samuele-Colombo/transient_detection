f6b621f5d5601d885a80980926a6e56d3de5ec7c
Samuele-Colombo <samuele.colombo1@studenti.unimi.it>
2023-02-16 04:50:57 -0500

    fix type issues

--- transient_detection/DeepLearning/scheduler.py
+++ transient_detection/DeepLearning/scheduler.py
@@ -2,9 +2,9 @@
 
 import numpy as np
 
-def cosine_scheduler(base_value, final_value, epochs, niter_per_ep, warmup_epochs=0, start_warmup_value=0):
+def cosine_scheduler(base_value: int, final_value: int, epochs: int, niter_per_ep: int, warmup_epochs: int = 0, start_warmup_value: int =0):
     warmup_schedule = np.array([])
-    warmup_iters = warmup_epochs * niter_per_ep
+    warmup_iters = int(warmup_epochs * niter_per_ep)
     if warmup_epochs > 0:
             warmup_schedule = np.linspace(start_warmup_value, base_value, warmup_iters)
 
