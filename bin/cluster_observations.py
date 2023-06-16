3fda80eeeebf67ab17bd2d156731aa773fedace4
Samuele Colombo <samuele.colombo1@studenti.unimi.it>
2023-06-16 13:23:10 +0200

    Fix clustering

--- bin/cluster_observations.py
+++ bin/cluster_observations.py
@@ -9,6 +9,7 @@ from tqdm import tqdm
 
 from transient_detection.DataPreprocessing.utilities import read_events, get_paired_filenames, in2d, get_uncompliant
 from main_parser import parse
+import multiprocessing
 
 def main():
 
@@ -27,16 +28,24 @@ def main():
     # genuine_files = np.array(glob(genuine_pattern))[:5]
     # simulated_files = np.array(glob(simulated_pattern))[:5]
     # are_mos = np.ones_like(genuine_files, dtype=bool)
-
     uncompliant_pairs = np.array(list(get_uncompliant(args["PATHS"]["compliance_file"])))
     gsnames = np.array(list(map(list, get_paired_filenames(basedir, genuine_pattern, simulated_pattern))))
     gsnames = gsnames[np.logical_not(in2d(gsnames, uncompliant_pairs))]
-    genuine_files, simulated_files = gsnames.T
+
+    # cluster_data(args, gsnames)
+
+    pool = multiprocessing.Pool()
+    ws = args["world_size"]
+    for rank in range(ws):
+        pool.apply_async(cluster_data, (args, gsnames, rank, ws))
+    pool.close()
+    pool.join()
+
+def cluster_data(args, gsnames, rank=0, world_size=1):
+    genuine_files, simulated_files = gsnames[rank::world_size].T
     are_mos = np.vectorize(lambda name: name.endswith("MIEVLI0000.FTZ"))(genuine_files)
 
-    clustered_bar = tqdm(total=len(are_mos))
-    counter = 0
-
+    clustered_bar = tqdm(total=len(are_mos), position=rank, desc="Clustering {}/{}".format(rank, world_size))
 
     for genuine_file, simulated_file, is_mos in zip(genuine_files, simulated_files, are_mos):
         sim_directory, sim_orginal_file = osp.split(simulated_file)
@@ -49,9 +58,8 @@ def main():
             if not osp.exists(new_sim_file) or not args["fast"]:
                 cluster_data.write(new_sim_file, format='fits', overwrite=True)
         clustered_bar.update(1)
-        counter += 1
-        # print("{}/{}: {:.2f}%".format(counter, len(are_mos), counter/len(are_mos)*100), end="\r")
         sys.stdout.flush()
+    clustered_bar.close()
 
 
 def cluster_data_generator(genuine_file, simulated_file, is_mos, args):
@@ -62,6 +70,7 @@ def cluster_data_generator(genuine_file, simulated_file, is_mos, args):
     # sim_events = events[events["ISSIMULATED"]].to_pandas().groupby(by=lastcolname, as_index=True, sort=True)
     sim_events = events[events["ISSIMULATED"]].to_pandas().set_index(lastcolname)
     for group_num in sim_events.index.unique():
+        group_num = int(group_num)
         df = sim_events.loc[group_num, keys]
         if len(df.shape) == 1: continue #ignore single photon transients, since useless
         extremes = torch.tensor(np.vstack([df.max(axis=0).values, df.min(axis=0).values]))
@@ -72,6 +81,19 @@ def cluster_data_generator(genuine_file, simulated_file, is_mos, args):
         # mask = torch.all(torched_events >= extremes[1], dim=1) & torch.all(torched_events <= extremes[0], dim=1)
         mask = (pd_events >= extremes[1]) & (pd_events <= extremes[0])
         masked_events = events[mask.all(axis=1).values]
+        new_keys = ["TIME", "X", "Y"]
+        for key in new_keys:
+            me_min = masked_events[key].min()
+            me_max = masked_events[key].max()
+            masked_events[key] = (masked_events[key] - me_min)/(me_max-me_min)
+            assert np.all(np.logical_and(masked_events[key] >= 0, masked_events[key] <= 1)), "masked events not finite {}".format(masked_events)
+
+        # pd_masked_events = masked_events.to_pandas()[new_keys]
+        # new_extremes = torch.tensor(np.vstack([pd_masked_events.max(axis=0).values, pd_masked_events.min(axis=0).values]))
+        # masked_events[new_keys].values = pd_masked_events = (torch.tensor(pd_masked_events.values) - new_extremes[1])/(new_extremes[0] - new_extremes[1])
+        # assert np.logical_and(pd_masked_events >= 0, pd_masked_events <= 1).all(), str(pd_masked_events)
+        # for colname in new_keys:
+        #     assert np.all(np.logical_and(masked_events[colname] >= 0, masked_events[colname] <= 1)), "masked events not finite {}".format(masked_events)
         # print(genuine_file, ": group", group_num)
         yield int(group_num), masked_events
 
